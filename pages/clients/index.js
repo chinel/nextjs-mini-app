ab7620ba7d16bd23d295159d4a6b984ade9fca2b
nelo <nelokwaz@gmail.com>
2021-11-12 14:11:30 +0100

    added dynamic links to the client page

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -10,6 +10,13 @@ function ClientsPage() {
   return (
     <div>
       <h1>The Clients Page</h1>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
