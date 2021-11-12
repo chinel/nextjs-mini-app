d5cb3455373fd36ec49766452eee7c56313e6f3f
nelo <nelokwaz@gmail.com>
2021-11-12 14:07:50 +0100

    added some dummy clients data

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,4 +1,12 @@
+import Link from "next/link";
 function ClientsPage() {
+  const clients = [
+    {
+      id: "max",
+      name: "Maximilian",
+    },
+    { id: "manu", name: "Manuel" },
+  ];
   return (
     <div>
       <h1>The Clients Page</h1>
