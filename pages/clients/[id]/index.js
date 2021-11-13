04a1ac295b65b9d4a461c68d6922ac7dac449eaa
nelo <nelokwaz@gmail.com>
2021-11-13 07:04:17 +0100

    added an onclick event handler to load projects programatically

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -1,7 +1,14 @@
+import { useRouter } from "next/router";
+
 function ClientsProjectsPage() {
+  const router = useRouter();
+  function loadProjectHandler() {
+    router.push("/clients/masx/projecta");
+  }
   return (
     <div>
       <h1>The proect of a given client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 }
