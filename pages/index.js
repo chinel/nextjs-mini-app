503318c8eb8fd75a02a8ddcbdad1135a441f46ec
nelo <nelokwaz@gmail.com>
2021-11-15 05:32:46 +0100

    retrieved the featured events from the dummy data for the homepage

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Link from "next/link";
+import { getFeaturedEvents } from "../dummy-data";
 function HomePage() {
+  const featuredEvents = getFeaturedEvents();
   return (
     <div>
       <h1>The Home Page</h1>
