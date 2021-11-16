61cda868c12cfba905bd05d955c41752dbf37bb8
nelo <nelokwaz@gmail.com>
2021-11-16 06:58:55 +0100

    added the date icon component to the event item component

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,6 +1,7 @@
 import Link from "next/Link";
 import Button from "../ui/button";
 import classes from "./event-item.module.css";
+import DateIcon from "../icons/date-icon";
 function EventItem(props) {
   const { title, image, date, location, id } = props;
   const humanReadableDate = new Date(date).toLocaleDateString("en-US", {
@@ -19,6 +20,7 @@ function EventItem(props) {
         <div className={classes.summary}>
           <h2>{title}</h2>
           <div className={classes.date}>
+            <DateIcon />
             <time>{humanReadableDate}</time>
           </div>
           <div className={classes.address}>
