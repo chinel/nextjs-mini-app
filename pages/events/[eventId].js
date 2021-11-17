b6bc2a7b876e98f0bd99cfc78a0405c5e399b530
nelo <nelokwaz@gmail.com>
2021-11-17 05:03:25 +0100

    added the event details components

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -5,6 +5,9 @@ function EventDetailPage() {
   const router = useRouter();
   const eventId = router.query.eventId;
   const event = getEventById(eventId);
+  if (!event) {
+    return <p>No event found!</p>;
+  }
   return (
     <div>
       <h1>Event Detail</h1>
