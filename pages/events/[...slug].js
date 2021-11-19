97df64bc7e159faff9ad9fc633eb8ef6f2c8e398
nelo <nelokwaz@gmail.com>
2021-11-19 20:22:54 +0100

    completed the event slug component

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,4 +1,5 @@
 import { useRouter } from "next/router";
+import EventList from "../../components/events/event-list";
 import { getFilteredEvents } from "../../dummy-data";
 
 function FilteredEventsPage() {
@@ -37,7 +38,7 @@ function FilteredEventsPage() {
 
   return (
     <div>
-      <h1>Filtered Events</h1>
+      <EventList items={filteredEvents} />
     </div>
   );
 }
