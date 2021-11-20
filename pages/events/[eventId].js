65ca7474bd03a1d32b4f98a137ffcf2d646b7479
nelo <nelokwaz@gmail.com>
2021-11-20 06:38:07 +0100

    added the error alert component to the event detail component

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -3,6 +3,7 @@ import { Fragment } from "react";
 import EventContent from "../../components/event-detail/event-content";
 import EventLogistics from "../../components/event-detail/event-logistics";
 import EventSummary from "../../components/event-detail/event-summary";
+import ErrorAlert from "../../components/ui/error-alert";
 import { getEventById } from "../../dummy-data";
 
 function EventDetailPage() {
@@ -10,7 +11,11 @@ function EventDetailPage() {
   const eventId = router.query.eventId;
   const event = getEventById(eventId);
   if (!event) {
-    return <p>No event found!</p>;
+    return (
+      <ErrorAlert>
+        <p>No event found!</p>
+      </ErrorAlert>
+    );
   }
   return (
     <Fragment>
