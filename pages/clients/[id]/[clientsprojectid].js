ca27d55700b6b99bd447bed25d4904f78e61ac73
nelo <nelokwaz@gmail.com>
2021-12-08 06:00:58 +0100

    removed unused pages

--- pages/clients/[id]/[clientsprojectid].js
+++
@@ -1,9 +0,0 @@
-function SelectedClientProjectPage() {
-  return (
-    <diiv>
-      <h1>The project page for a specific project for a selected client</h1>
-    </diiv>
-  );
-}
-
-export default SelectedClientProject;
