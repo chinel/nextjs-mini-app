7b2bbe58d611719798fdf0ff2b1214ebe173ec77
nelo <nelokwaz@gmail.com>
2021-12-10 06:35:54 +0100

    added the logic to get event detail

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -3,7 +3,7 @@ import EventContent from "../../components/event-detail/event-content";
 import EventLogistics from "../../components/event-detail/event-logistics";
 import EventSummary from "../../components/event-detail/event-summary";
 import ErrorAlert from "../../components/ui/error-alert";
-import { getEventById } from "../../helpers/api-util";
+import { getEventById, getAllEvents } from "../../helpers/api-util";
 
 function EventDetailPage(props) {
   const event = props.selectedEvent;
@@ -37,6 +37,16 @@ export async function getStaticProps(context) {
     props: {
       selectedEvent: event,
     },
+  };
+}
+
+export async function getStaticPaths() {
+  const events = await getAllEvents();
+  const paths = events.map((event) => ({ params: { eventId: event.id } }));
+
+  return {
+    paths: paths,
+    fallback: false, // here because we have all possible eventId, we will set its value as false.  So that it can through a 404 error page if we try to retrieve an id that doesn't exist'
   };
 }
 
