44e7f9f85bec42649a1930aab55de96d4cbefba4
nelo <nelokwaz@gmail.com>
2021-12-12 21:26:41 +0100

    added title and meta tag to the slug page

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -5,7 +5,7 @@ import ResultsTitle from "../../components/events/results-title";
 import Button from "../../components/ui/button";
 import ErrorAlert from "../../components/ui/error-alert";
 import { getFilteredEvents } from "../../helpers/api-util";
-
+import Head from "next/head";
 function FilteredEventsPage(props) {
   const router = useRouter();
 
@@ -41,6 +41,13 @@ function FilteredEventsPage(props) {
 
   return (
     <Fragment>
+      <Head>
+        <title>Filtered Events</title>
+        <meta
+          name="description"
+          content={`All events for ${numMonth}/${numYear}`}
+        />
+      </Head>
       <ResultsTitle date={date} />
       <EventList items={filteredEvents} />
     </Fragment>
