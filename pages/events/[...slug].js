685f53ac20d3ff91965b97c2b46e042d273f980f
nelo <nelokwaz@gmail.com>
2021-12-12 04:31:01 +0100

    added error handling from the server side to the client side

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -6,7 +6,7 @@ import Button from "../../components/ui/button";
 import ErrorAlert from "../../components/ui/error-alert";
 import { getFilteredEvents } from "../../dummy-data";
 
-function FilteredEventsPage() {
+function FilteredEventsPage(props) {
   const router = useRouter();
   const filterData = router.query.slug;
 
@@ -20,14 +20,7 @@ function FilteredEventsPage() {
   const numYear = +filteredYear;
   const numMonth = +filteredMonth;
 
-  if (
-    isNaN(numYear) ||
-    isNaN(numMonth) ||
-    numYear > 2030 ||
-    numYear < 2021 ||
-    numMonth < 1 ||
-    numMonth > 12
-  ) {
+  if (props.hasError) {
     return (
       <Fragment>
         <ErrorAlert>
@@ -66,6 +59,46 @@ function FilteredEventsPage() {
       <EventList items={filteredEvents} />
     </Fragment>
   );
+}
+
+export async function getServerSideProps(context) {
+  const { params } = context;
+  const filterData = params.slug;
+  const filteredYear = filterData[0];
+  const filteredMonth = filterData[1];
+
+  const numYear = +filteredYear;
+  const numMonth = +filteredMonth;
+
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth < 1 ||
+    numMonth > 12
+  ) {
+    return {
+      props: {
+        hasError: true,
+      },
+      //we can also use a not found set to true but we will be using a custom prop to indicate an error occurred
+      //notFound: true,
+      //Below if we have a custom error page we can redirect to that instead of using the custom 404 page
+      // redirect: {
+      //   destination: "/error",
+      // },
+    };
+  }
+
+  const filteredEvents = getFilteredEvents({
+    year: numYear,
+    month: numMonth,
+  });
+
+  return {
+    props: {},
+  };
 }
 
 export default FilteredEventsPage;
