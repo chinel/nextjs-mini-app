c0278d5a388eb9277557616e1fe287df0c2f03d4
nelo <nelokwaz@gmail.com>
2021-12-14 18:28:35 +0100

    completed the structure of the custom document file

---
+++ pages/_document.js
@@ -0,0 +1,15 @@
+import Document, { Head, Main, Html, NextScript } from "next/document";
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+        <body>
+          <div id="overlays"></div>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
