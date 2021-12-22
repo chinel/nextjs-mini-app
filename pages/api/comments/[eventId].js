9787704ab275c3dd96c53ee4006c8379736e5971
nelo <nelokwaz@gmail.com>
2021-12-22 06:56:47 +0100

    added base validation for the comment post data

--- pages/api/comments/[eventId].js
+++ pages/api/comments/[eventId].js
@@ -1,5 +1,21 @@
 function handler(req, res) {
+  const eventId = req.query.eventId;
   if (req.method === "POST") {
+    const { email, name, text } = req.body;
+    //Add server side validation
+    if (
+      !email.includes("@") ||
+      !name ||
+      name.trim() === "" ||
+      !text ||
+      text.trim() === ""
+    ) {
+      res.status(422).json({ message: "Invalid Input" });
+      return;
+    }
+  }
+
+  if (req.method === "GET") {
   }
 }
 
