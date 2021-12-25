d089c1e28293dd13b1260c4ad3bd69402702db8d
nelo <nelokwaz@gmail.com>
2021-12-25 08:26:04 +0100

    changed  the database and table name in the newsletter api route

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -9,11 +9,11 @@ async function handler(req, res) {
     }
 
     const client = await MongoClient.connect(
-      "mongodb+srv://nextDbUser:nextdbpassword@cluster-nextjs.yaea0.mongodb.net/newsletter?retryWrites=true&w=majority"
+      "mongodb+srv://DbUser:dbpassword@cluster-nextjs.yaea0.mongodb.net/events?retryWrites=true&w=majority"
     );
 
     const db = client.db(); //here we can pass the database name as a string to this db method, but since we already specified it in the database url above we can leave it
-    await db.collection("emails").insertOne({
+    await db.collection("newsletter").insertOne({
       email: userEmail,
     });
     client.close();
