b0cbfd66ca3c6747f066e3905a18ec2c92afaf57
nelo <nelokwaz@gmail.com>
2021-12-25 12:09:28 +0100

    completed the database logic for the comments api route

--- pages/api/comments/[eventId].js
+++ pages/api/comments/[eventId].js
@@ -2,7 +2,7 @@ import { MongoClient } from "mongodb";
 async function handler(req, res) {
   const eventId = req.query.eventId;
   const client = await MongoClient.connect(
-    "mongodb+srv://nextDbUser:nextdbpassword@cluster-nextjs.yaea0.mongodb.net/events?retryWrites=true&w=majority"
+    "mongodb+srv://DbUser:dbpassword@cluster-nextjs.yaea0.mongodb.net/events?retryWrites=true&w=majority"
   );
   if (req.method === "POST") {
     const { email, name, text } = req.body;
