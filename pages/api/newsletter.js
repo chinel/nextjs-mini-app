1b1087b935d879577cd3185c20d8f4206c764e57
nelo <nelokwaz@gmail.com>
2021-12-25 07:54:24 +0100

    added the logic to insert the user email to database

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -8,6 +8,16 @@ function handler(req, res) {
       return;
     }
 
+    MongoClient.connect(
+      "mongodb+srv://nextDbUser:nextdbpassword@cluster-nextjs.yaea0.mongodb.net/newsletter?retryWrites=true&w=majority"
+    )
+      .then((client) => {
+        const db = client.db(); //here we can pass the database name as a string to this db method, but since we already specified it in the database url above we can leave it
+        return db.collection("emails").insertOne({
+          email: userEmail,
+        });
+      })
+      .then((data) => {});
     console.log(userEmail);
     res.status(201).json({ message: "Signed up!" });
   }
