f2e009a748f2c0e02d154cf5e2ffa3e028b8f529
nelo <nelokwaz@gmail.com>
2021-12-25 22:21:52 +0100

    added error handling to the comments api route

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -9,9 +9,7 @@ async function connectToDatabase() {
 
 async function insertDocument(client, document) {
   const db = client.db(); //here we can pass the database name as a string to this db method, but since we already specified it in the database url above we can leave it
-  await db.collection("newsletter").insertOne({
-    email: userEmail,
-  });
+  await db.collection("newsletter").insertOne(document);
 }
 
 async function handler(req, res) {
@@ -23,7 +21,25 @@ async function handler(req, res) {
       return;
     }
 
-    client.close();
+    let client;
+
+    try {
+      client = connectToDatabase();
+    } catch (error) {
+      res.status(500).json({ message: "Connecting to the database failed" });
+      return;
+    }
+
+    try {
+      await insertDocument(client, {
+        email: userEmail,
+      });
+      client.close();
+    } catch (error) {
+      res.status(500).json({ message: "Inserting data failed" });
+      return;
+    }
+
     res.status(201).json({ message: "Signed up!" });
   }
 }
