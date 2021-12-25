78cda04464805a634e30b9c3feeda00ac6f3ad73
nelo <nelokwaz@gmail.com>
2021-12-25 22:50:53 +0100

    created a reusable function to insert  document

--- pages/api/comments/[eventId].js
+++ pages/api/comments/[eventId].js
@@ -46,12 +46,6 @@ async function handler(req, res) {
   }
 
   if (req.method === "GET") {
-    const db = client.db();
-    const documents = await db
-      .collection("comments")
-      .find({ eventId: eventId })
-      .sort({ _id: -1 }) //-1 to sort the id in descending order and +1 to sort the id in ascending order;
-      .toArray();
     res.status(200).json({ comments: documents });
   }
   client.close();
