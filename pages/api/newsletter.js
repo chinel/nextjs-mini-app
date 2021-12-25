ce56367692fcb6f8e95f9e767edc7e444435ea59
nelo <nelokwaz@gmail.com>
2021-12-25 08:06:05 +0100

    switched from promise then to async await int he newsletter api route

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,5 +1,5 @@
 import { MongoClient } from "mongodb";
-function handler(req, res) {
+async function handler(req, res) {
   if (req.method === "POST") {
     const userEmail = req.body.email;
 
@@ -8,17 +8,15 @@ function handler(req, res) {
       return;
     }
 
-    MongoClient.connect(
+    const client = await MongoClient.connect(
       "mongodb+srv://nextDbUser:nextdbpassword@cluster-nextjs.yaea0.mongodb.net/newsletter?retryWrites=true&w=majority"
-    )
-      .then((client) => {
-        const db = client.db(); //here we can pass the database name as a string to this db method, but since we already specified it in the database url above we can leave it
-        return db.collection("emails").insertOne({
-          email: userEmail,
-        });
-      })
-      .then((data) => {});
-    console.log(userEmail);
+    );
+
+    const db = client.db(); //here we can pass the database name as a string to this db method, but since we already specified it in the database url above we can leave it
+    await db.collection("emails").insertOne({
+      email: userEmail,
+    });
+    client.close();
     res.status(201).json({ message: "Signed up!" });
   }
 }
