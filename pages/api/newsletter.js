db93e5f14df3399326fc7eaac0f032a4ffd8a41d
nelo <nelokwaz@gmail.com>
2021-12-25 22:11:35 +0100

    created a helper function to connect to the database

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,4 +1,11 @@
 import { MongoClient } from "mongodb";
+
+function connectToDatabase() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://DbUser:dbpassword@cluster-nextjs.yaea0.mongodb.net/events?retryWrites=true&w=majority"
+  );
+}
+
 async function handler(req, res) {
   if (req.method === "POST") {
     const userEmail = req.body.email;
@@ -7,10 +14,6 @@ async function handler(req, res) {
       res.status(422).json({ message: "Invalid email addresss" });
       return;
     }
-
-    const client = await MongoClient.connect(
-      "mongodb+srv://DbUser:dbpassword@cluster-nextjs.yaea0.mongodb.net/events?retryWrites=true&w=majority"
-    );
 
     const db = client.db(); //here we can pass the database name as a string to this db method, but since we already specified it in the database url above we can leave it
     await db.collection("newsletter").insertOne({
