283e7bbe4b4379653ce99926891c685c144bf9f0
nelo <nelokwaz@gmail.com>
2021-12-26 18:25:21 +0100

    added the logic to automatically close the notfication bar after 3 seconds

--- store/notification-context.js
+++ store/notification-context.js
@@ -9,6 +9,24 @@ const NotificationContext = createContext({
 export function NotificationContextProvider(props) {
   const [activeNotification, setActiveNotification] = useState();
 
+  useEffect(() => {
+    if (
+      activeNotification &&
+      (activeNotification.status === "success" ||
+        activeNotification.status === "error")
+    ) {
+      const timer = setTimeout(() => {
+        setActiveNotification(null);
+      }, 3000);
+
+      //we have to use the cleanup function to clear the time out, just incase the useEffect reruns before the timeout expires
+      //so that we do no have a queue of setTimeouts
+      return () => {
+        clearTimeout(timer);
+      };
+    }
+  }, [activeNotification]);
+
   function showNotificationHandler(notificationData) {
     setActiveNotification(notificationData);
   }
